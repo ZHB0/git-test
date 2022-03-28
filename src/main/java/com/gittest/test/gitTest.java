d8e41dd9d7d2428227b9160f7b48ef7b979e350c
ZHB <zhb@qq.com>
2022-03-28 17:21:44 +0800

    first commit

---
+++ src/main/java/com/gittest/test/gitTest.java
@@ -0,0 +1,12 @@
+package com.gittest.test;
+
+/**
+ * @author ZHB
+ * @create 2022-03-28 17:17
+ */
+public class gitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
